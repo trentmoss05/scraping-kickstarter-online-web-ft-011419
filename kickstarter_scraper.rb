f13fbe318d132556d656757dcd5bf75fd7d1d92d
Trent Moss <trentmoss05@gmail.com>
2019-01-31 04:18:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,10 +8,10 @@ def create_project_hash
   binding.pry
 end
 
-# kickstarter.css("li.project.grid_4").first
+# projects : kickstarter.css("li.project.grid_4").first
 # title : project.css("h2.bbcard_name strong a").text
 # image : project.css("div.project-thumbnail a img").attribute("src").value
 # description : project.css("p.bbcard_blurb").text
 # location : project.css("span.location-name").text
-
+# funded : project.css("ul.project-stats li.first.funded strong").text
 create_project_hash
