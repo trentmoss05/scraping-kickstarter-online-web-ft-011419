24e59cb0d8114d6844a23409fc8683826abbda25
Trent Moss <trentmoss05@gmail.com>
2019-01-31 04:19:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
-
+  projects
 end
 
 # projects : kickstarter.css("li.project.grid_4").first
